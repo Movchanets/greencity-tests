d5674b7021f6a625b051901a49a62670f845bf13
movchanets <slavikmovchanetc@gmail.com>
2026-04-30 16:50:20 +0300

    Add Allure steps for improved test reporting and refactor tests for clarity

--- pages/components/event_card_components.py
+++ pages/components/event_card_components.py
@@ -1,4 +1,5 @@
 
+import allure
 from selenium.webdriver.common.by import By
 from pages.components.base_component import BaseComponent
 
@@ -7,11 +8,13 @@ class EventCardComponent(BaseComponent):
     more_button_locator = (By.CSS_SELECTOR, ".secondary-global-button, .event-button")
     name_locator = (By.CSS_SELECTOR, ".event-title, .event-name, p")
 
+    @allure.step("Open event details from card")
     def click_more(self):
         more_button = self.find_element(*self.more_button_locator)
         self.scroll_to(more_button)
         self.driver.execute_script("arguments[0].click();", more_button)
 
+    @allure.step("Get event name from card")
     def get_name(self):
         name_element = self.find_element(*self.name_locator)
         return name_element.text.strip()
